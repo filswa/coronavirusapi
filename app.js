a77ecaf1a9ee78c883e91dc058fefca59035b424
Filip Swacha <filip.swacha@gmail.com>
2020-02-04 01:25:41 +0100

    App.js: Update port for HEROKU

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var port = process.env.PORT || 3000;
+
 global.XMLHttpRequest = require('xhr2');
 const express = require('express');
 const app = express();
@@ -18,4 +20,4 @@ app.get('/', (req, res) => {
 //     res.send();
 // })
 
-app.listen(3000);
+app.listen(port);
