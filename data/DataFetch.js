1359adceb606d240a6590ee9fcfa5981ef5c4b5b
filswa <filip.swacha@gmail.com>
2020-02-11 11:57:13 +0100

    DataFetch: Update data url

--- data/DataFetch.js
+++ data/DataFetch.js
@@ -1,14 +1,18 @@
 const Papa = require('papaparse');
+const Axios = require('axios');
 const DataStore = require('./DataStore');
 const WorldStats = require('./WorldStats');
 const PlacesStats = require('./PlacesStats');
 const CountriesStats = require('./CountriesStats');
 
+let dataURL = ""
+let githubUrl = "https://github.com/CSSEGISandData/2019-nCoV/tree/master/daily_case_updates";
+
 let virusData = {};
 
 function FetchDataFromCsv(){
     console.log("Fetching data...");
-    Papa.parse("https://raw.githubusercontent.com/CSSEGISandData/2019-nCoV/master/daily_case_updates/02-10-2020_1030.csv",{
+    Papa.parse("https://raw.githubusercontent.com/CSSEGISandData/2019-nCoV/master/daily_case_updates/02-10-2020_1930.csv",{
             download: true,
             complete: function(results){
                 virusData = results.data;
@@ -21,13 +25,24 @@ function FetchDataFromCsv(){
 
                 // console.log(virusData);
                 // console.log(countriesData);
-                
+
                 DataStore.setWorldData(worldData);
                 DataStore.setPlacesData(placesData);
                 DataStore.setCountriesData(countriesData);
             }
         });
 }; FetchDataFromCsv();
+
+function getlatestDataUrl(){
+    Axios.get(githubUrl)
+      .then(response => {
+        console.log(response.data);
+        //return response.json();
+      })
+      .catch(error => {
+        console.log(error);
+      });
+}
 
 function getVirusData(){
     return virusData;
